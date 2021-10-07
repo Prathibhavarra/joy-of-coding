205d77f8d04adb7f679bfb48b807223ada886061
Prathibhavarra <85438383+Prathibhavarra@users.noreply.github.com>
2021-10-07 22:05:46 +0530

    completed the website

---
+++ main (4).c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+   int num=22,a,b,c,d;
+   a=num<<1;
+   printf("the value of num is:%d\n",a);
+    b=num<<2;
+    printf("the value of num is:%d\n",b);
+    c=num>>1;
+    printf("the value of num is:%d\n",c);
+    d=num>>2;
+     printf("the value of num is:%d\n",d);
+    return 0;
+}
---
+++ main (5).c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+   int a=50,b=64,largest;
+   largest=(a>b)?a:b;
+   printf("largest number is %d\n",largest);
+   
+
+    return 0;
+}
